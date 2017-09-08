aadc6a75135a3b298535e2842c2f6825d27c01ed
Muhammad Ubaid Raza <mubaidr@gmail.com>
2017-09-08 15:46:57 +0500

    create db and convertion modules

--- app/db-service.js
+++ app/db-service.js
@@ -1,25 +1,27 @@
 var mongoDB = require('mongodb')
 var mongoClient = mongoDB.MongoClient()
 var dbURL = 'mongodb://root:root@ds127854.mlab.com:27854/url-short-service'
+// move user/pass to system configuration
 
 module.exports = {
-  getURL (short) {
-    mongoClient.connect(dbURL, (err, db) => {
+  async getURL (short) {
+    var database = null
+    var query = {
+      short: short
+    }
+
+    await mongoClient.connect(dbURL, (err, db) => {
       if (err) throw err
+      database = db
+    })
 
-      var query = {
-        address: 'Park Lane 38'
-      }
-      db.collection('urls').find(query).toArray((err, result) => {
-        if (err) throw err
-
-        console.log(result)
-
-        db.close()
-      })
+    await database.collection('urls').find(query).toArray((err, result) => {
+      if (err) throw err
+      database.close()
+      return result[0]
     })
   },
-  setURL (url, short) {
-
+  async setURL (url, short) {
+    // implement url update
   }
 }
--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var path = require('path')
 var url = require('url')
 
-var urlService = require('./url-service')
+// var urlService = require('./url-service')
 var dbService = require('./db-service')
 
 var app = express()
@@ -21,6 +21,9 @@ app.use('*', (req, res, next) => {
 
 app.get('/:code', (req, res, next) => {
   let code = req.params.code
+  let obj = dbService.getURL(code)
+  // redirect to url
+  console.log('got: ', obj)
 })
 
 app.get('/', (req, res, next) => {
@@ -33,10 +36,16 @@ app.get('/', (req, res, next) => {
     })
   } else {
     var short = null
+    // calculate short code
+    let obj = dbService.getURL(short)
+    if (!obj) {
+      // save short, url to db
+    }
+
     res.send({
       error: null,
-      url: address,
-      short: short
+      url: obj.url,
+      short: obj.short
     })
   }
 })

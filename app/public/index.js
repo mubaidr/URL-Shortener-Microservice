f3c33914a31d66b21d754588c067fe30df65ec84
Muhammad Ubaid Raza <mubaidr@gmail.com>
2017-09-13 19:39:32 +0500

    Finalize front end, add vue code

---
+++ app/public/index.js
@@ -0,0 +1,19 @@
+// eslint-disable-next-line
+Vue.config.productionTip = false
+// eslint-disable-next-line
+var app = new Vue({
+  el: '#app',
+  data () {
+    return {
+      url: 'https://github.com/mubaidr',
+      short: 'Your Result will appear here!',
+      loading: false
+    }
+  },
+  methods: {
+    shortenURL () {
+
+    }
+  },
+  mounted () {}
+})
